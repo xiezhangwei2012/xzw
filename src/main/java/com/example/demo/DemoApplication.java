7ca90edec11532f46c3437606c72144fec79c569
xiezhangwei <xiezhangwei@zbj.com>
2019-05-19 23:13:26 +0800

    edit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
-@MapperScan("com.example.demo.mapper")
+@MapperScan("com.example.demo.dao.mapper")
+@ComponentScan("com.example.demo.*")
 public class DemoApplication {
     @RequestMapping("/demo")
     String helloWorld(){
